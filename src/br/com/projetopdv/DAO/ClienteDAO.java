1a4443abd615e826ccc61af8506a713b8b32506b
Roger Reichert <rogereichert@gmail.com>
2019-09-18 10:14:10 -0300

    Criação da classe ClienteDAO, estrutura base com seus métodos

---
+++ src/br/com/projetopdv/DAO/ClienteDAO.java
@@ -0,0 +1,24 @@
+package br.com.projetopdv.DAO;
+
+/**
+ * @author Roger Reichert
+ * @version 1.0.0
+ */
+public class ClienteDAO {
+   
+    public void consultaCliente(){
+        
+    }
+    
+    public void cadastraCliente(){
+        
+    }
+    
+    public void desativaCliente(){
+        
+    }
+    
+    public void atualizaCliente(){
+        
+    }
+}
