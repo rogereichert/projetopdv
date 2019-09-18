b2788c22732f33603ae9dca86324856a06c74276
Roger Reichert <rogereichert@gmail.com>
2019-09-18 10:08:56 -0300

    Criação da model Cliente

---
+++ src/br/com/projetopdv/model/Cliente.java
@@ -0,0 +1,91 @@
+package br.com.projetopdv.model;
+
+/**
+ * @author Roger Reichert
+ * @version 1.0.0
+ */
+public class Cliente {
+    
+    private int idCliente;
+    private String nome;
+    private String cpf;
+    private String cep;
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String telefone;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+   
+}
