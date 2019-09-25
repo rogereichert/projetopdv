5cf606bdc5bf66d33db15524d820c9c060dafb7d
Roger Reichert <rogereichert@gmail.com>
2019-09-25 13:43:11 -0300

    Criação da classe Funcionario

---
+++ src/br/com/projetopdv/model/Funcionario.java
@@ -0,0 +1,55 @@
+package br.com.projetopdv.model;
+
+/**
+ * @author Roger Reichert
+ */
+public class Funcionario extends Pessoa {
+    
+    private int id;
+    private int id_pessoa;
+    private String login;
+    private String senha;
+    private String perfil;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_pessoa() {
+        return id_pessoa;
+    }
+
+    public void setId_pessoa(int id_pessoa) {
+        this.id_pessoa = id_pessoa;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+    
+    
+}
