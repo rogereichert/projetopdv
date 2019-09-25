5927f14ae1018121d1f632be2182d7cbeafb2765
Roger Reichert <rogereichert@gmail.com>
2019-09-25 13:38:19 -0300

    Criação da classe ItemVenda

---
+++ src/br/com/projetopdv/model/ItemVenda.java
@@ -0,0 +1,54 @@
+package br.com.projetopdv.model;
+
+/**
+ * @author Roger Reichert
+ */
+public class ItemVenda {
+ 
+    private int id;
+    private Venda venda;
+    private Produto produto;
+    private int quantidade;
+    private double subtotal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+}
