91f73c795b88454a274b4668f18f28acb1e78b2c
Roger Reichert <rogereichert@gmail.com>
2019-09-27 13:56:50 -0300

    Adicionado FRMmenuPrincipal e pequenos ajustes na tela FRMcadastroUnidade

--- src/br/com/projetopdv/view/FRMcadastroUnidade.java
+++ src/br/com/projetopdv/view/FRMcadastroUnidade.java
@@ -69,7 +69,7 @@ public class FRMcadastroUnidade extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowActivated(java.awt.event.WindowEvent evt) {
                 formWindowActivated(evt);
